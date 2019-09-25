b6068a1e211426733f5c792969f7310f82bfe946
bisnukundu <bisnukundu@gmail.com>
2019-09-25 09:30:40 +0600

    Updates

---
+++ precache-manifest.e0c0657198a859b8f3551497e11f2371.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0aebe62b40ecd378452099c9ed58110e",
+    "url": "/mobile_shop/index.html"
+  },
+  {
+    "revision": "0bddc40c2f543d8ee94e",
+    "url": "/mobile_shop/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3123282b0137b857deaa",
+    "url": "/mobile_shop/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "0bddc40c2f543d8ee94e",
+    "url": "/mobile_shop/static/js/2.7e74d44c.chunk.js"
+  },
+  {
+    "revision": "3123282b0137b857deaa",
+    "url": "/mobile_shop/static/js/main.0d499b60.chunk.js"
+  },
+  {
+    "revision": "1357b2c5de09f501e377",
+    "url": "/mobile_shop/static/js/runtime~main.97f23c84.js"
+  },
+  {
+    "revision": "b4a30bdded68c3915952977cb25fdf06",
+    "url": "/mobile_shop/static/media/ios-cart.b4a30bdd.svg"
+  }
+]);
